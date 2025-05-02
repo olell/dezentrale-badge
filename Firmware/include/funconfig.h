b807306f240a8cd2963cc087eb09a506ef301962
olel <olel@olel.de>
2025-05-02 19:57:11 +0200

    Enabled PLL

--- Firmware/include/funconfig.h
+++ Firmware/include/funconfig.h
@@ -1,6 +1,8 @@
 #ifndef _FUNCONFIG_H
 #define _FUNCONFIG_H
 
+#define FUNCONF_USE_HSI 1    		// internal 24MHz clock oscillator
+#define FUNCONF_USE_PLL 1			// use PLL x2
 #define RANDOM_STRENGTH    2
 #define FUNCONF_SYSTICK_USE_HCLK 1
 #define CH32V003           1
