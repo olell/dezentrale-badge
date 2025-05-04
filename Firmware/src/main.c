f2d5443ec4e31b9c0c8974c745a50878afc2248b
olel <gitlab+dezentrale@olel.de>
2025-05-04 13:53:01 +0200

    impl button interrupt

--- Firmware/src/main.c
+++ Firmware/src/main.c
@@ -13,17 +13,48 @@
 #include "animations/face.h"
 
  // max milliseconds between two pressed to be counted as multi-press
-#define MULTI_PRESS_SPEED 400
+#define MULTI_PRESS_SPEED 500
 
 size_t current_animation_idx;
 animation_t* current_animation;
 
-uint8_t loop();
+// button interrupt handler
+uint32_t btn_last_press = 0;
+uint32_t btn_press = 0;
+uint32_t btn_duration = 0;
+uint32_t now = 0;
+uint8_t btn_cnt = 0;
+
+void EXTI7_0_IRQHandler( void ) __attribute__((interrupt));
+void EXTI7_0_IRQHandler( void ) {
+    now = millis();
+
+    // debounce code
+    if (now - btn_press < 50) {
+        EXTI->INTFR = EXTI_Line4;
+        return;
+    };
+
+    btn_last_press = btn_press;
+    btn_press = now;
+
+    if (btn_cnt == 0) btn_cnt = 1;
+
+    // count how many time the button was pressed shortly
+    if (btn_press - btn_last_press < MULTI_PRESS_SPEED) {
+        btn_cnt += 1;
+    }
+
+    EXTI->INTFR = EXTI_Line4;
+}
 
 int main() {
     /* System init functions */
     SystemInit();
-    funGpioInitAll();
+    
+    funGpioInitC();
+    funGpioInitD();
+
     systickInit();
     matrixInit();
 
@@ -31,6 +62,14 @@ int main() {
     standby_gpio_init();
     standby_gpio_assign_button(GPIOD, 4);
     standby_gpio_assign_pin(STANDBY_GPIO_PORT_D, 4, STANDBY_TRIGGER_DIRECTION_FALLING);
+
+    // setup button interrupt
+	AFIO->EXTICR = AFIO_EXTICR_EXTI4_PD;
+	EXTI->INTENR = EXTI_INTENR_MR4;
+	EXTI->RTENR = EXTI_RTENR_TR4;
+    EXTI->FTENR &= ~EXTI_FTENR_TR4;
+
+    NVIC_EnableIRQ( EXTI7_0_IRQn );
 
     // register animations
     register_animation(&face_animation);
@@ -48,8 +87,35 @@ int main() {
     current_animation->init();
 
     for (;;) {
-        // repeat the loop until it returns 0
-        while (loop());
+
+        // repeat the loop until it breaks to sleep
+        for (;;) {
+            // Matrix animation
+            current_animation->tick();
+
+            Delay_Ms(10); // sleep 10ms to reduce cpu load
+            
+            // btn pressed at least once and more than MULTI_PRESS_SPEED ago
+            // press count is captured by the interrupt handler
+            if (btn_cnt > 0 && millis() - btn_press > MULTI_PRESS_SPEED) {
+                if (btn_cnt == 4) { // go to sleep
+                    btn_cnt = 0;
+                    break;
+                }
+                else if (btn_cnt == 3) { // next animation
+                    matrixClear();
+                    current_animation_idx = (current_animation_idx + 1) % get_animation_count();
+                    current_animation = get_animation(current_animation_idx);
+                    current_animation->init();
+                }
+                else {
+                    current_animation->on_press(btn_cnt);
+                }
+                btn_cnt = 0;
+            }
+
+        }
+
         // disable the systick & turn off the matrix
         systickClear();
         matrixPowerOff();
@@ -61,63 +127,4 @@ int main() {
         systickInit();
         Delay_Ms(1000);
     }
-}
-
-uint8_t btn = 1, btn_prev = 1;
-uint32_t btn_last_press = 0;
-uint32_t btn_press = 0;
-uint32_t btn_duration = 0;
-uint8_t btn_cnt = 0;
-
-/**
- * @brief repeats until it returns 0 then the controller goes to deep sleep
- */
-uint8_t loop() {
-    // Matrix animation
-    current_animation->tick();
-
-    Delay_Ms(10); // sleep 10ms to reduce cpu load
-
-    // do some actions if the button was pressed
-    btn_prev = btn;
-    btn = funDigitalRead(PD4);
-    if (btn_prev && !btn) { // "falling edge"
-        // store the time when the button was pressed / last time pressed
-        btn_last_press = btn_press;
-        btn_press = millis();        
-        // wait until the button was released
-        while (!funDigitalRead(PD4));
-
-        // store the time when the button was released
-        btn_duration = millis() - btn_press;
-
-        // if the button wasnt pressed long, it was
-        // pressed at least once short
-        if (btn_cnt == 0) btn_cnt = 1;
-
-        // count how many time the button was pressed shortly
-        if (btn_press - btn_last_press < MULTI_PRESS_SPEED) {
-            btn_cnt += 1;
-        }
-    }
-
-    // btn pressed at least once and more than MULTI_PRESS_SPEED ago
-    if (btn_cnt > 0 && millis() - btn_press > MULTI_PRESS_SPEED) {
-        if (btn_cnt == 4) { // go to sleep
-            btn_cnt = 0;
-            return 0;
-        }
-        else if (btn_cnt == 3) { // next animation
-            matrixClear();
-            current_animation_idx = (current_animation_idx + 1) % get_animation_count();
-            current_animation = get_animation(current_animation_idx);
-            current_animation->init();
-        }
-        else {
-            current_animation->on_press(btn_cnt);
-        }
-        btn_cnt = 0;
-    }
-
-    return 1;
 }
