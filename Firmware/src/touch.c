1542764c9f6c468d0ec085c13486ede406da574e
olel <gitlab+dezentrale@olel.de>
2025-06-19 15:17:51 +0200

    ⚡️ changed to fixed point arithmetic

--- Firmware/src/touch.c
+++ Firmware/src/touch.c
@@ -3,7 +3,7 @@
 
 #define TOUCH_CHANNELS 2
 
-#define FABS(x) ((x) < 0 ? -(x) : (x))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 
 touch_channel_t channels[TOUCH_CHANNELS] = {
     {
@@ -40,10 +40,10 @@ void touchUpdate() {
     for (uint8_t i = 0; i < TOUCH_CHANNELS; i ++) {
         touch_reading_t *r = &readings[i];
 
-        r->current = ReadTouchPin(channels[i].port , channels[i].pin, channels[i].adc, 3);
+        r->current = ReadTouchPin(channels[i].port, channels[i].pin, channels[i].adc, 3) * 100;
 
         if (r->baseline == 0) r->baseline = r->current;
-        else r->baseline = r->baseline * 0.99 + r->current * 0.01;
+        else r->baseline = (r->baseline / 100) * 99 + (r->current / 100);
         
         r->max = r->current > r->max ? r->current : r->max;
     }
@@ -58,12 +58,12 @@ uint8_t touchRead(uint8_t channel) {
     touch_reading_t *r = &readings[channel];
 
     // check for plausible baseline/max ratio
-    float touch_diff = 1 - (r->max / r->baseline);
-    float norm;
-    if (FABS(touch_diff) < 0.2) { // invalid reading
+    int touch_diff = 100 - ((r->max*100) / r->baseline);
+    int norm;
+    if (ABS(touch_diff) < 20) { // invalid reading
         norm = 0;
     } else {
-        norm = (r->current - r->baseline) / (r->max - r->baseline) * 255.0;
+        norm = ((r->current - r->baseline)*255) / (r->max - r->baseline);
         norm = norm > 255 ? 255 : norm;
         norm = norm < 0 ? 0 : norm;
     }
